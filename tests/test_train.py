df3b93cbd78a787f5159386f35ce7e31047cd91a
AlphaSahiphoulayeSY <38104794+AlphaSahiphoulayeSY@users.noreply.github.com>
2025-03-20 16:41:39 +0100

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,26 +1,26 @@
 import os
-from joblib import load
+import joblib
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-def check_pickle_file_exists(picklefile):
-  assert os.path.exists(picklefile), (
-    "Le fichier trained_model.pkl est introuvable"
-  )
+def test_train_model_file_exists():
+    """Vérifie que le fichier churn_model_clean.pkl est créé après exécution de train.py"""
+    assert os.path.exists('data/churn_model_clean.pkl'), (
+        "Le fichier churn_model_clean.pkl n'existe pas après l'exécution de train.py."
+    )
 
-def check_input_file_exists(csvfile):
-  assert os.path.exists(csvfile), (
-    "Le fichier csv d'entrée est introuvable"
-  )
+def test_train_model_loading():
+    """Vérifie que le fichier sauvegardé contient un modèle Random Forest"""
+    model = joblib.load('data/churn_model_clean.pkl')
+    assert isinstance(model, LogisticRegression), (
+        "Le fichier churn_model_clean.pkl ne contient pas un modèle LogisticRegression."
+    )
 
-def test_train_model_loading(model):
-  assert isinstance(model, LogisticRegression), (
-    "Le modèle n'est pas une régression logistique" 
-  )
+def test_train_model_prediction():
+    """Vérifie que le modèle entraîné peut prédire sur un sous-ensemble des données"""
+    model = joblib.load('data/churn_model_clean.pkl')
+    data = pd.read_csv('data/customer_churn.csv')
+    X = data[['Age', 'Account_Manager', 'Years', 'Num_Sites']]
 
-picklefile = "data/trained_model.pkl"
-csvfile = "data/customer_churn.csv"
-model = load('data/trained_model.pkl')
-
-check_pickle_file_exists(picklefile)
-check_input_file_exists(csvfile)
-test_train_model_loading(model)
+    prediction = model.predict(X[:1])
+    assert prediction is not None, "Le modèle n'a pas retourné de prédiction."
